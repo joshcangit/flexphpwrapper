e5f57697dfea2a706c98f04a9b22c9434975dbb4
joshcangit <33022160+joshcangit@users.noreply.github.com>
2019-01-22 14:04:03 +0000

    Update alias.php

--- pdo/alias.php
+++ pdo/alias.php
@@ -1,23 +1,23 @@
 <?php
 if (version_compare(PHP_VERSION, '5.1', '>=')) {
-    define('PBOOL', PDO::PARAM_BOOL);
-    define('PNULL', PDO::PARAM_NULL);
-    define('PINT', PDO::PARAM_INT);
-    define('PSTR', PDO::PARAM_STR);
+    define('PB', PDO::PARAM_BOOL);
+    define('PN', PDO::PARAM_NULL);
+    define('PI', PDO::PARAM_INT);
+    define('PS', PDO::PARAM_STR);
     if (version_compare(PHP_VERSION, '7.2.0', '>=')) {
-        define('PSTRN', PDO::PARAM_STR_NATL);
-        define('PSTRC', PDO::PARAM_STR_CHAR);
+        define('PSN', PDO::PARAM_STR_NATL);
+        define('PSC', PDO::PARAM_STR_CHAR);
     }
-    define('PLOB', PDO::PARAM_LOB);
-    define('PSTMT', PDO::PARAM_STMT);
+    define('PL', PDO::PARAM_LOB);
+    define('PST', PDO::PARAM_STMT);
     define('PIO', PDO::PARAM_INPUT_OUTPUT);
 } else {
-    define('PBOOL', PDO_PARAM_BOOL);
-    define('PNULL', PDO_PARAM_NULL);
-    define('PINT', PDO_PARAM_INT);
-    define('PSTR', PDO_PARAM_STR);
-    define('PLOB', PDO_PARAM_LOB);
-    define('PSTMT', PDO_PARAM_STMT);
+    define('PB', PDO_PARAM_BOOL);
+    define('PN', PDO_PARAM_NULL);
+    define('PI', PDO_PARAM_INT);
+    define('PS', PDO_PARAM_STR);
+    define('PL', PDO_PARAM_LOB);
+    define('PST', PDO_PARAM_STMT);
     define('PIO', PDO_PARAM_INPUT_OUTPUT);
 }
 ?>
